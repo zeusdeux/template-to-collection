7f2b1e4e1b9d81c103e81980b81fb725aa0c75e6
Mudit Ameta <zeusdeux@gmail.com>
2016-02-04 22:52:22 +0530

    Format code

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 /*
  * Return true if there are any non-empty strings in the array
  */
+
 const anyKeysAsStrings = arrOfKeys => !!arrOfKeys.join('').replace(/\s/g, '').length
+
 
 /*
  * Create the "right" kind of Object
  */
+
 const createObject = (strKeys, ...vals) => {
   let o = Object.create(null)
   const keysAsStrings = anyKeysAsStrings(strKeys)
@@ -20,9 +23,11 @@ const createObject = (strKeys, ...vals) => {
   return o
 }
 
+
 /*
  * Create Map or WeakMap
  */
+
 const _createMaps = (Constructor, strKeys, vals) => {
   let m = new Constructor()
   const keysAsStrings = anyKeysAsStrings(strKeys)
@@ -35,14 +40,18 @@ const _createMaps = (Constructor, strKeys, vals) => {
   return m
 }
 
+
 /*
  * Sugar to create Map
  */
+
 const createMap = (strKeys, ...vals) => _createMaps(Map, strKeys, vals)
+
 
 /*
  * Sugar to create WeakMap
  */
+
 const createWMap = (strKeys, ...vals) => _createMaps(WeakMap, strKeys, vals)
 
 
